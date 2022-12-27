e6301efc7729f0a72f431194c19713c79a0b4376
abcd1234 <90763140+BackHong10@users.noreply.github.com>
2022-12-27 14:23:48 +0900

    fixUser

--- src/apis/category/user/dto/user-create.input.ts
+++
@@ -1,5 +0,0 @@
-import { InputType, OmitType } from '@nestjs/graphql';
-import { User } from '../entities/user.entity';
-
-@InputType()
-export class CreateUserInput extends OmitType(User, ['id', 'age'], InputType) {}

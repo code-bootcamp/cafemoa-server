4c76528804b31862bfe96851da3b1adafe0a6121
김태현 <eiiyy12@nate.com>
2022-12-27 14:11:24 +0900

    feature#-user

---
+++ src/apis/category/user/user.resolver.ts
@@ -0,0 +1,43 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateUserInput } from './dto/user-create.input';
+import { UpdateUserInput } from './dto/user-update.input';
+import { User } from './entities/user.entity';
+import { UserService } from './user.service';
+
+@Resolver()
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => User)
+  async createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput,
+  ): Promise<User> {
+    return await this.userService.create({
+      createUserInput,
+    });
+  }
+
+  @Query(() => [User])
+  fetchUsers(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Query(() => User)
+  fetchUser(@Args('email') email: string): Promise<User> {
+    return this.userService.find({ email });
+  }
+
+  @Mutation(() => User)
+  async updateUser(
+    @Args('updateUserInput') updateUserInput: UpdateUserInput, //
+  ) {
+    return await this.userService.update({
+      updateUserInput,
+    });
+  }
+
+  @Mutation(() => Boolean)
+  deleteUser(@Args('userId') userId: string): Promise<boolean> {
+    return this.userService.delete({ userId });
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryModule } from './apis/category/category.module';
+import { UserModule } from './apis/category/user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    UserModule,
     CategoryModule,
     ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({

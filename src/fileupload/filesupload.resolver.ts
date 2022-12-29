943d8c5e2c19dff224397256be942d4b3e3bc912
김상현 <gimsanghyeon@gimsanghyeon-ui-MacBookAir.local>
2022-12-29 21:42:55 +0900

    feat#image

---
+++ src/fileupload/filesupload.resolver.ts
@@ -0,0 +1,17 @@
+import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { FileUpload, GraphQLUpload } from "graphql-upload";
+import { FilesUploadService } from "./filesupload.service";
+
+@Resolver()
+export class FilesUploadResolver{
+    constructor(
+        private readonly fileUploadService: FilesUploadService,//
+        ){}
+
+    @Mutation(() => [String])
+    uploadFile(
+        @Args({ name: 'files', type: () => [GraphQLUpload] }) files: FileUpload[],
+    ) {
+      return this.fileUploadService.upload({ files });
+    }
+}

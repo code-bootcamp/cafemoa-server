0e8980f4f580ece0744f7925c7c11472e486cf1d
abcd1234 <90763140+BackHong10@users.noreply.github.com>
2022-12-29 10:19:22 +0900

    Revert "Feature#docker / logout "

--- src/apis/user/dto/user-update.input.ts
+++
@@ -1,5 +0,0 @@
-import { InputType, PartialType } from '@nestjs/graphql';
-import { CreateUserInput } from './user-create.input';
-
-@InputType()
-export class UpdateUserInput extends PartialType(CreateUserInput) {}

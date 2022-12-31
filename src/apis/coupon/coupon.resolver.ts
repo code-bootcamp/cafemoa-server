a84e15c38a4f21c0ecde405fe9516ca463cf2cf4
김태현 <eiiyy12@nate.com>
2022-12-31 19:19:15 +0900

    fear#쿠폰사용API

--- src/apis/coupon/coupon.resolver.ts
+++ src/apis/coupon/coupon.resolver.ts
@@ -1,4 +1,6 @@
+import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { GqlAuthAccessGuard } from 'src/commons/auth/gql-auth.guard';
 import { CouponService } from './coupon.service';
 import { CreateCouponInput } from './dto/coupon-create.input';
 import { Coupon } from './entities/coupon.entity';
@@ -9,26 +11,31 @@ export class CouponResolver {
     private readonly couponService: CouponService, //
   ) {}
 
+  @UseGuards(GqlAuthAccessGuard)
   @Query(() => Coupon)
   fetchCoupon(@Args('couponId') couponId: string) {
     return this.couponService.find({ couponId });
   }
 
+  @UseGuards(GqlAuthAccessGuard)
   @Query(() => [Coupon])
   fetchCoupons() {
     return this.couponService.findAll();
   }
 
+  @UseGuards(GqlAuthAccessGuard)
   @Query(() => [Coupon])
   fetchUserCoupons(@Args('userId') userId: string) {
     return this.couponService.findUserCoupon({ userId });
   }
 
+  @UseGuards(GqlAuthAccessGuard)
   @Query(() => [Coupon])
   fetchCafeCoupons(@Args('cafeId') cafeId: string) {
     return this.couponService.findCafeCoupon({ cafeId });
   }
 
+  @UseGuards(GqlAuthAccessGuard)
   @Mutation(() => Coupon)
   createCoupon(
     @Args('createCouponInput') createCouponInput: CreateCouponInput,
@@ -36,6 +43,16 @@ export class CouponResolver {
     return this.couponService.createCoupon({ createCouponInput });
   }
 
+  @UseGuards(GqlAuthAccessGuard)
+  @Mutation(() => String)
+  useCoupon(
+    @Args('couponId') couponId: string,
+    @Args('password') password: string,
+  ) {
+    return this.couponService.useCoupon({ couponId, password });
+  }
+
+  @UseGuards(GqlAuthAccessGuard)
   @Mutation(() => String)
   deleteCoupon(@Args('couponId') couponId: string) {
     return this.couponService.deleteCoupon({ couponId });
